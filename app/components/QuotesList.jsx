f874d2157f4cc131fcd7306524b02c80f8b5d3d5
Nicholas Vanderpool <nvanderp@nicholass-mbp.attlocal.net>
2017-12-17 19:51:14 -0500

    Fixing poem formatting

--- app/components/QuotesList.jsx
+++ app/components/QuotesList.jsx
@@ -6,15 +6,15 @@ function QuotesList(props) {
 
     return (
         <div className='poem-container'>
-            {/* <h3>Poem</h3>
-            <div className='poem'>
+            <h3>Poem</h3>
+            <pre className='poem'>
                 { 
                     !poem 
                     ? 'Loading poem...' 
                     : poem.content 
                 }
-            </div> */}
-            <h3>Available Quotes</h3>
+            </pre>
+            {/* <h3>Available Quotes</h3>
             {
                 !quotes
                 ? null
@@ -25,15 +25,40 @@ function QuotesList(props) {
                         </div>
                     )
                 })
-            }
+            } */}
         </div>
     )
 }
 
+// utility function
+const pickPoem = (quotes) => {
+    let index = Math.floor(Math.random() * quotes.length)
+    let quote = quotes[index]
+    let newQuote = Object.assign({}, quote)
+    newQuote.content = cummings(quote.content)
+    return newQuote
+}
+
+const cummings = (words = '') => {
+	let resultStr = ''
+	words.split('').forEach(word => {
+		const cummingsSpaces = Math.random() * 300
+		let cummingsSpace = '\t'
+		for (let i = 0; i < cummingsSpaces; i++) {
+			cummingsSpace += '\t'
+		}
+		resultStr += word + cummingsSpace
+	})
+
+	return resultStr
+}
+
 const mapStateToProps = function(state, ownProps) {
+    let poem
+    if (state.quotes.length) { poem = pickPoem(state.quotes) }
     return {
         quotes: state.quotes,
-        // poem: state.poem
+        poem
     }
 }
 
